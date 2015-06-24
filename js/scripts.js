e569048ec754a6d865461dd88813aa47235a8748
Ben Cornelis <bencornelis02@gmail.com>
2015-06-24 11:31:39 -0700

    add youtube api page token crawler

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,40 @@
-$(function() {
-  $("#ajax").click(function() {
-    var city = $("#city").val();
-    $.ajax({
-      type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/weather",
-      data: {
-        q: city,
-        units: "imperial"
-      },
-      success: function(data) {
-        $("#response").append("<ul class='list-unstyled'>" +
-                                "<li>City: " + data.name + "</li>" +
-                                "<li>Temperature: " + data.main.temp + "</li>" +
-                                "<li>Humidity: " + data.main.humidity + "</li>" +
-                              "</ul>");
-      }
-    });
+API_KEY = "AIzaSyDSW307ilh8iZL2gFIqR6R_nOoGpBoSc_Y";
+
+var url = "https://www.googleapis.com/youtube/v3/search?maxResults=50&type=video&order=viewCount&part=snippet";
+url += "&key=" + API_KEY;
+
+var videoUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics";
+videoUrl += "&key=" + API_KEY;
+
+function buildTokenURL(pageToken) {
+  return url + "&pageToken=" + pageToken;
+}
+
+function buildVideoURL(videoId) {
+  return videoUrl + "&id=" + videoId;
+}
+
+var i = 0;
+function getNextPage(pageToken) {
+  $.getJSON(buildTokenURL(pageToken)).then(function(responseJSON) {
+    i += 1;
+    var firstId = responseJSON.items[0].id.videoId;
+    findViewCount(firstId);
+
+    var nextPageToken = responseJSON.nextPageToken;
+    console.log(nextPageToken);
+    if (i < 100) {getNextPage(nextPageToken)};
   });
+}
+
+function findViewCount(videoId) {
+  $.getJSON(buildVideoURL(videoId)).then(function(responseJSON) {
+    console.log("Page " + i + " view count: " + responseJSON.items[0].statistics.viewCount);
+  })
+}
+
+$(function() {
+  $.getJSON(url).then(function(responseJSON) {
+    getNextPage(responseJSON.nextPageToken);
+  })
 });
